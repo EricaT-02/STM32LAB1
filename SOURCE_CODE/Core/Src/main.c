793ff582d4899929c4c4988a903947bf754257e1
phuongtrinhh_HCMUT <phuongtrinhh0212@gmail.com>
2025-09-14 00:50:58 +0700

    Update Ex2

--- SOURCE_CODE/Core/Src/main.c
+++ SOURCE_CODE/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,15 +84,39 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int counter = -1;
   while (1)
   {
-	  //TODO EX2
+	 if (counter <= 0) {
+		 counter = 9;
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+	 } else if (counter > 5) {
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+		 counter--;
+	 } else if (counter > 3 && counter <= 5) {
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+		 counter--;
+	 } else if (counter <= 3 && counter > 0){
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+		 counter--;
+	 }
+
+	 HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -132,6 +157,30 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
